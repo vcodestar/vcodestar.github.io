b19dcb50ce93ab470b9ea6fc2091bb7f4febebda
vcodestar <vaggelischasanis@gmail.com>
2023-07-09 02:12:47 +0300

    update

--- script1.js
+++ script1.js
@@ -1,8 +1,26 @@
 var sidebar = document.getElementById('sidebar');
+var opn = false;
+var tgl = document.getElementById("toggleButton");
+var isClass1 = false;
+
 
 function toggleSidebar() {
   sidebar.classList.toggle('open');
-  console.log("Hello");
+  opn = !opn;
+  console.log(opn)
 }
 
+
+tgl.addEventListener("click", function() {
+  if (isClass1) {
+    tgl.innerHTML = '<i class="fa fa-caret-right"></i>';
+    tgl.classList.remove("class2");
+    tgl.classList.add("class1");
+  } else {
+    tgl.innerHTML = '<i class="fa fa-caret-left"></i>';
+    tgl.classList.remove("class1");
+    tgl.classList.add("class2");
+  }
+  isClass1 = !isClass1;
+});
 
