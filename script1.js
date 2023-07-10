7514c31d84494527bca27e211c20852e2fc974a5
vcodestar <vaggelischasanis@gmail.com>
2023-07-10 21:10:01 +0300

    update

--- script1.js
+++ script1.js
@@ -5,6 +5,22 @@ var isClass1 = false;
 tgl.classList.add("class1");
 
 
+var dropdownButtons = document.querySelectorAll('.dropdown-btn');
+dropdownButtons.forEach(function(button) {
+  button.addEventListener('click', function() {
+    var dropdown = this.parentNode;
+    dropdown.classList.toggle('show');
+  });
+});
+
+// Hide the dropdown content when the cursor hovers outside the dropdown container
+var dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(function(dropdown) {
+  dropdown.addEventListener('mouseleave', function() {
+    this.classList.remove('show');
+  });
+});
+
 function toggleSidebar() {
   sidebar.classList.toggle('open');
   opn = !opn;
