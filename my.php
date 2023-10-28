0fd91bd7f9b924f2f5b9cc1aafd56f2dd1179174
vcodestar <vaggelischasanis@gmail.com>
2023-10-28 23:13:51 +0300

    update

---
+++ my.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html>
+<title>VC</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<link rel="stylesheet" href="W3S.css">
+<link rel="stylesheet" href="style1.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://kit.fontawesome.com/967252b7d1.js" crossorigin="anonymous"></script>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.13.0/css/all.css"
+      integrity="sha384-Bfad6CLCknfcloXFOyFnlgtENryhrpZCe29RTifKEixXQZ38WheV+i/6YWSzkz3V"
+      crossorigin="anonymous"
+    />
+    <link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+<body>
+
+  <div class="particles">
+
+    <span style="--i:11;"></span>
+    <span style="--i:12;"></span>
+    <span style="--i:24;"></span>
+    <span style="--i:10;"></span>
+    <span style="--i:14;"></span>
+    <span style="--i:23;"></span>
+    <span style="--i:18;"></span>
+    <span style="--i:16;"></span>
+    <span style="--i:19;"></span>
+    <span style="--i:20;"></span>
+    <span style="--i:22;"></span>
+    <span style="--i:25;"></span>
+    <span style="--i:18;"></span>
+    <span style="--i:21;"></span>
+    <span style="--i:13;"></span>
+    <span style="--i:15;"></span>
+    <span style="--i:26;"></span>
+    <span style="--i:17;"></span>
+    <span style="--i:13;"></span>
+    <span style="--i:28;"></span>
+    <span style="--i:11;"></span>
+    <span style="--i:12;"></span>
+    <span style="--i:24;"></span>
+    <span style="--i:10;"></span>
+    <span style="--i:14;"></span>
+    <span style="--i:23;"></span>
+    <span style="--i:18;"></span>
+    <span style="--i:16;"></span>
+    <span style="--i:19;"></span>
+    <span style="--i:20;"></span>
+    <span style="--i:22;"></span>
+    <span style="--i:25;"></span>
+    <span style="--i:18;"></span>
+    <span style="--i:21;"></span>
+    <span style="--i:13;"></span>
+    <span style="--i:15;"></span>
+    <span style="--i:26;"></span>
+    <span style="--i:17;"></span>
+    <span style="--i:13;"></span>
+    <span style="--i:28;"></span>
+
+  </div>
+
+  <button id="toggleButton" onclick="toggleSidebar()">
+    <i class="fa-solid fa-list icon"></i>
+  </button>
+
+<div class="sidebar" id="sidebar">
+  
+  <div class="dropdown">
+    <a href="index.html" class="home-btn"><i class="fa fa-home" aria-hidden="true"></i></a> 
+    <div class="dropdown-btn">
+      <button class="w3-button">Java
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="javapda.html" class="w3-bar-item w3-button">PDA</a>
+        <a href="WordListHTML.html" class="w3-bar-item w3-button">Word List Counter </a>
+        <a href="KthSmallestNumberHTML.html" class="w3-bar-item w3-button">K-th Smallest Number In Unsorted Array </a>
+        <a href="MemoryGame.html" class="w3-bar-item w3-button">Memory Game(PvP & AI) </a>
+        <a href="CardGame.html" class="w3-bar-item w3-button">Card Game(versus AI)</a>
+        <a href="CubesProblem.html" class="w3-bar-item w3-button">Cubes Problem(AI Solution)</a>
+      </div>
+    </div>
+  </div>
+    
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">C
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="C/ReaderWriter.c" class="w3-bar-item w3-button">ReaderWriter.c</a>
+      </div>
+    </div> 
+  </div>
+
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">Python
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="CodingBat.html" class="w3-bar-item w3-button">Coding Bat</a>
+        <a href="Kaggle.html" class="w3-bar-item w3-button">Kaggle</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">Haskell
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="Haskell/Lab3.hs" class="w3-bar-item w3-button" >Lab 3(2022)</a>
+        <a href="Haskell/Lab4.hs" class="w3-bar-item w3-button" >Lab 4(2022)</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">Risc-V Assembly
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="Assembly/PositionDetector/PositionDetector.s" download="PositionDetector.s" class="w3-bar-item w3-button">Position Detector (Lab 7 2022)</a>
+      </div>
+    </div>
+  </div>
+  
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">ROS 2
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="https://drive.google.com/drive/u/1/folders/1T7PNVPjMOQArZHmZbvn4AxCWtztIDei-" class="w3-bar-item w3-button">package(TurtleBot3)</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">Leetcode
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="Leetcode/MedianOfTwoSortedaArrays/MedianOfTwoSortedArrays.java" download="MedianOfTwoSortedaArrays.java" class="w3-bar-item w3-button">MedianOfTwoSortedArrays.java</a>
+        <a href="Leetcode/TwoSum.java" download="TwoSum.java" class="w3-bar-item w3-button">TwoSum.java</a>
+        <a href="Leetcode/RemoveNthNode.java" download="RemoveNthNode.java" class="w3-bar-item w3-button">RemoveNthNode.java</a>
+        <a href="Leetcode/PowerfulIntegers.c" download="PowerfulIntegers.c" class="w3-bar-item w3-button">PowerfulIntegers.c</a>
+        <a href="Leetcode/powXN.c" download="powXN.c" class="w3-bar-item w3-button">powXN.c</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">GNU Octave
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="ComputationalMathematics.html" class="w3-bar-item w3-button">Computational Mathematics (2022-2023)</a>
+        <a href="Octave/difference_table.m" download="difference_table.m" class="w3-bar-item w3-button">difference_table.m</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="dropdown">
+    <div class="dropdown-btn">
+      <button class="w3-button">TNI (Team Project)
+        <i class="fa fa-caret-down"></i>
+      </button>
+      <div class="dropdown-content">
+        <a href="tni/demo.html" class="w3-bar-item w3-button">Demo Gameplay</a>
+        <a href="https://www.linkedin.com/in/militsa-voudouri-2b8a16278" target="_blank" class="w3-bar-item w3-button">Militsa Voudouri</a>
+        <a href="https://www.linkedin.com/in/kelly-oikonomou-036289276" target="_blank" class="w3-bar-item w3-button">Kaliopi Oikonomou</a>
+        <a href="https://www.linkedin.com/in/katerina-papaxristopoulou-740221219" target="_blank" class="w3-bar-item w3-button">Aikaterini Papachristopoulou</a>
+      </div>
+    </div>
+  </div>
+
+  
+  
+  
+</div>
+
+<div class="centered-text">
+  <h1>VC</h1>
+</div>
+
+
+<div>
+    <footer>
+        <!---<a href="#"><i class="fab fa-facebook"></i></a>-->
+        <a href="https://www.linkedin.com/in/evangelos-chasanis-708610280/"><i class="fab fa-linkedin"></i></a>
+        <a href="https://github.com/vcodestar"><i class="fab fa-github"></i></a>
+        <a href="https://www.instagram.com/chasanis_/"><i class="fab fa-instagram"></i></a>
+        <!--<a href="#"><i class="fas fa-info"></i></a>
+        <a href="#"><i class="fas fa-share-alt"></i></a>-->
+        <!--<i class="fa fa-apple"></i>-->
+    </footer>
+</div>
+
+<script src="script1.js"></script>
+
+</body>
+</html>
