bc8a6ef71b35fd6362d73184c3de7bbc8b96f671
vcodestar <vaggelischasanis@gmail.com>
2023-10-29 14:35:00 +0200

    update

--- uoi_practice/script.js
+++ uoi_practice/script.js
@@ -8,6 +8,7 @@ function sendmail() {
     let fullName = document.getElementById("name").value;
     let userEmail = document.getElementById("email").value;
     let userMessage = document.getElementById("message").value;
+    let btn = document.getElementById("btn");
     
         var contactParams = {
             
@@ -18,5 +19,17 @@ function sendmail() {
         };
 
         console.log("Sending email to :", contactParams.to_email);
-        emailjs.send('service_vc4real', 'template_bea9gba', contactParams).then(function (res) {})
+        emailjs.send('service_vc4real', 'template_bea9gba', contactParams)
+        .then(function (res) {
+            // Clear input fields after sending the email
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+
+            btn.style.display = "none";
+            btn.disabled = true;
+
+            // Show the success message
+            document.getElementById("successMessage").style.display = "block";
+        });
 }
