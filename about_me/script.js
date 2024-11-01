3ed4c59d8126b5a5cbdd28d497acaae297a47670
vcodestar <vaggelischasanis@gmail.com>
2024-11-01 12:41:58 +0200

    update

--- about_me/script.js
+++ about_me/script.js
@@ -1,13 +1,28 @@
 const texts = [
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    "It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.",
-    "More recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    `<h1 class="headerTitle">Who am I?</h1>
+    <p id="iam">
+        I am a senior undergraduate student in the Department of Computer Science and Engineering at the University of Ioannina, maintaining a GPA of 7.61/10 and currently just one class away from graduation. My primary interests lie in systems programming, application development, and artificial intelligence, where I have gained a strong blend of theoretical knowledge and hands-on experience. Throughout my studies and personal projects, I’ve worked to develop skills in software system design, machine learning applications, and creating innovative solutions to computational challenges.
+    </p>
+    `,
+    `<h1 class="headerTitle">Experience</h1>
+    <ul>
+        <li>
+            <strong>Internship at Terracom SA</strong>
+            <p>
+                During my internship at Terracom SA, I focused on two main areas: automation testing and web development. I worked with <strong>Cypress</strong> for end-to-end testing, creating automated test scripts to ensure the reliability and efficiency of the software. This involved writing, executing, and debugging tests to validate various user interactions and flows within the applications.
+            </p>
+            <p>
+                In addition to testing, I contributed to <strong>web development</strong> projects using <strong>Angular</strong>. My responsibilities included developing dynamic and responsive user interfaces, implementing front-end components, and collaborating with team members to enhance application functionality. This experience strengthened my skills in JavaScript, TypeScript, and modern front-end frameworks.
+            </p>
+        </li>
+    </ul>
+    `,
 ];
-let currentIndex = 1;
+
+let currentIndex = -1;
 
 function updateText() {
-    document.getElementById('text-content').innerText = texts[currentIndex];
+    document.getElementById('text-content').innerHTML = texts[currentIndex];
 }
 
 function nextText() {
